13b3a7c87e832362065ab8029e18b0289faf37d7
Michael Reichenbach <michael.reichenbach@interhyp.de>
2021-01-14 16:04:32 +0100

    chore: add basic art structure

---
+++ src/main/java/de/raidcraft/achievements/listener/RewardListener.java
@@ -0,0 +1,30 @@
+package de.raidcraft.achievements.listener;
+
+import de.raidcraft.achievements.events.PlayerUnlockedAchievementEvent;
+import io.artframework.ArtContext;
+import io.artframework.Scope;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class RewardListener implements Listener {
+
+    private final Scope scope;
+    private final Map<UUID, ArtContext> rewards = new HashMap<>();
+
+    public RewardListener(Scope scope) {
+
+        this.scope = scope;
+    }
+
+    @EventHandler(ignoreCancelled = true)
+    public void onUnlocked(PlayerUnlockedAchievementEvent event) {
+
+        ArtContext context = rewards.computeIfAbsent(event.achievement().id(), uuid -> scope.load(event.achievement().rewards()));
+
+        context.execute(event.playerAchievement());
+    }
+}
