cfefe8412db454c1d5578b6f4b9ba2fab888a518
Silthus <michael@reichenbach.in>
2021-03-11 21:22:14 +0100

    fix: do not use . for the achievement alias

--- src/main/java/de/raidcraft/achievements/util/ConfigUtil.java
+++ src/main/java/de/raidcraft/achievements/util/ConfigUtil.java
@@ -17,8 +17,8 @@ public class ConfigUtil {
 
         Path relativePath = base.relativize(file.toPath());
         return relativePath.toString()
-                .replace("/", ".")
-                .replace("\\", ".")
+                .replace("/", "-")
+                .replace("\\", "-")
                 .toLowerCase()
                 .replace(".yml", "")
                 .replace(".yaml", "");
